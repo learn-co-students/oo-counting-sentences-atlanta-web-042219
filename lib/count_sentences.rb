c1a0a3ad4a818c02f4fb1c44328240fef90252db
Will Jones <willjones2010@gmail.com>
2019-04-24 22:57:59 -0400

    9 examples 2 failures

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+  if self.end_with?(".")
+    true
+  else
+    false
+   end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
-  def exclamation?
 
+  def exclamation?
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+  self.split("!"||"?"||".")
+    if self == []
+      return 0
+    else
+      return self.length
+    end
   end
 end
